2403eeba919c125d65adbf99eb2a807b90a9fda6
aldimisbahul234 <aldimisbahul234@gmail.com@aldimisbahul234.noreply.github.com>
2021-07-25 20:38:08 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 <body>
 
   <!-- ======= Top Bar ======= -->
-  <div id="topbar" class="d-flex align-items-center fixed-top">
+  <!-- <div id="topbar" class="d-flex align-items-center fixed-top">
     <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
       <div class="align-items-center d-none d-md-flex">
         <i class="bi bi-clock"></i> Monday - Saturday, 8AM to 10PM
@@ -49,7 +49,7 @@
         <i class="bi bi-phone"></i> Call us now +1 5589 55488 55
       </div>
     </div>
-  </div>
+  </div> -->
 
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
