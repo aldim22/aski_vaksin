c247fd7e3ad57caa3678d4efa60e7bb393cd3083
aldimisbahul234 <aldimisbahul234@gmail.com@aldimisbahul234.noreply.github.com>
2021-07-26 14:42:40 +0700

    fix

--- resources/views/formRegister.blade.php
+++ resources/views/formRegister.blade.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="ASKI VAKSIN">
 	<meta name="author" content="">
-
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 
     <title>Form Registrasi</title>
