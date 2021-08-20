9a61cd0802100e53645a696602a923364356a5d2
aldim22 <62983273+aldim22@users.noreply.github.com>
2021-08-20 08:39:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,6 @@ Route::get('/download/{nik}', 'UploadController@download');
 Route::get('/registrasi',"formRegisterController@index") -> name('getForm');
 Route::post('/registrasi',"formRegisterController@search") -> name('getFormSearch');
 Route::get('/registrasi/count',"formRegisterController@counterP") -> name('getFormCounter');
-Route::get('/registrasi/count121',"formRegisterController@counter121") -> name('getFormCounter121');
-Route::get('/registrasi/count221',"formRegisterController@counter221") -> name('getFormCounter221');
-Route::get('/registrasi/count122',"formRegisterController@counter122") -> name('getFormCounter122');
-Route::get('/registrasi/count222',"formRegisterController@counter222") -> name('getFormCounter222');
 Route::get('/registrasi/status',"formRegisterController@indexStatus") -> name('getFormStatus');
 Route::get('/registrasi/status/0',"formRegisterController@excel0") -> name('getFormStatus0');
 Route::get('/registrasi/status/1',"formRegisterController@excel1") -> name('getFormStatus1');
@@ -37,3 +33,8 @@ Route::get('/registrasi/status/3',"formRegisterController@excel3") -> name('getF
 Route::get('/registrasi/status/4',"formRegisterController@excel4") -> name('getFormStatus4');
 Route::get('/registrasi/status/5',"formRegisterController@excel5") -> name('getFormStatus5');
 
+Route::get('/check', 'UploadController@check');
+Route::get('/list', 'UploadController@list');
+Route::get('/edit/{id}', 'UploadController@edit');
+Route::get('/delete_peserta/{id}', 'UploadController@delete_peserta');
+Route::post('/update_peserta/{id}', 'UploadController@update_peserta');
